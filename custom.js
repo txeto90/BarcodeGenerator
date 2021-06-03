f686f0cd3e47853f4ac1c50207c599fdda5dab39
Jose Monzonis <herpessenyor@gmail.com>
2021-06-03 09:22:22 +0200

    second commit

---
+++ custom.js
@@ -0,0 +1,52 @@
+function calculateCode(value){
+    var appId = 90;
+    var formatType = 523;
+    //clave emisora
+    var prov = 46;
+    var mun = 131;
+    var controlDigit = 3;
+    //numero justificante
+    var modelCode = 047;
+    var dataId = 4;
+    let v = "" + value;
+    
+    if(value.length != 8){
+        let str = "0";	
+        for (let i = 0; i < 7-v.length; i++) {
+            str += "0";
+        }
+        value = str + value;				
+    }
+    console.log("value" + value);
+    var secuentialNum = value; //"00058461";
+
+    //concats
+    var emisor = "461313";
+    var justNum = "0474" + secuentialNum;
+    var controlInt = (parseInt(emisor, 10) + parseInt(justNum, 10)) % 7;
+
+    if(controlInt === 0){
+        return "" + appId + formatType + emisor + justNum + "7";
+    }else{
+        return "" + appId + formatType + emisor + justNum + controlInt;
+    }
+}
+
+function generateBarcode(){
+    var from = document.getElementById("fromValue").value;
+    var to = document.getElementById("toValue").value;	
+
+    for (let i = from; i < to; i++) {				
+        //905234613130474000584616
+        $('#theDiv').prepend('<svg id="barcode'+i+'"></svg>')
+        
+         JsBarcode("#barcode"+i, calculateCode(i), {
+            format: "code128c",
+            displayValue: true,
+            lineColor: "#24292e",
+            width:1.5,
+            height:40,	
+            fontSize: 10					
+        });	 
+    }
+}
