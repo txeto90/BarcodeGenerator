c151e0d6b421bac3191da0d5a72437c850b7883c
Jose Monzonis <herpessenyor@gmail.com>
2021-06-04 13:01:36 +0200

    suitable for printing in pages

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
-function calculateCode(value){
+
+function calculateCode(value) {
     var appId = 90;
     var formatType = 523;
     //clave emisora
@@ -9,15 +10,15 @@ function calculateCode(value){
     var modelCode = 047;
     var dataId = 4;
     let v = "" + value;
-    
-    if(value.length != 8){
-        let str = "0";	
-        for (let i = 0; i < 7-v.length; i++) {
+    let year = new Date();
+
+    if (value.length != 6) {
+        let str = "0";
+        for (let i = 0; i < 5 - v.length; i++) {
             str += "0";
         }
-        value = str + value;				
+        value = year.getFullYear().toString().substr(-2) + str + value;
     }
-    console.log("value" + value);
     var secuentialNum = value; //"00058461";
 
     //concats
@@ -25,37 +26,40 @@ function calculateCode(value){
     var justNum = "0474" + secuentialNum;
     var controlInt = (parseInt(emisor, 10) + parseInt(justNum, 10)) % 7;
 
-    if(controlInt === 0){
+    if (controlInt === 0) {
         return "" + appId + formatType + emisor + justNum + "7";
-    }else{
+    } else {
         return "" + appId + formatType + emisor + justNum + controlInt;
     }
 }
 
-function generateBarcode(){
-    var from = document.getElementById("fromValue").value;
-    var to = document.getElementById("toValue").value;	
+function generateBarcode() {
 
-    if(from >= to){
+    var from = document.getElementById("fromValue").value;
+    var amount = parseInt(from, 10) + parseInt(document.getElementById("amount").value, 10);
+
+    if (from >= amount) {
         alert("El nombre inicial es més gran que el nombre de fi");
     }
 
-    for (let i = from; i < to; i++) {				
+    for (let i = amount; i >= from; i--) {
         //905234613130474000584616
-        $('#theDiv').prepend('<svg id="barcode'+i+'"></svg>')
-        
-         JsBarcode("#barcode"+i, calculateCode(i), {
-            format: "code128c",
+        $('#theDiv').prepend('<svg id="barcode' + i + '"></svg>')
+
+        JsBarcode("#barcode" + i, calculateCode(i), {
+            format: "code128",
             displayValue: true,
             lineColor: "#24292e",
-            width:1.5,
-            height:40,	
-            fontSize: 10					
-        });	 
+            width: 1.5,
+            height: 40,
+            fontSize: 10
+        });
     }
     $("#btnSubmit").attr("disabled", true);
+    $("#inputForm").remove();
 }
 
-$('input').change(function(){
+$('input').change(function () {
     $("#btnSubmit").attr("disabled", false);
 })
+
